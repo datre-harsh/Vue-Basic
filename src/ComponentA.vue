6ec93ff9b1cb820f043e13c553d15fd251d3220a
datre-harsh <h.datre@tcu.edu>
2026-03-13 18:26:43 -0500

    Vue Intro

---
+++ src/ComponentA.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="background-color: lightgreen; padding: 5px">
+    Child component.
+    <ComponentB></ComponentB>
+    <ComponentB></ComponentB>
+    <ComponentB></ComponentB>
+  </div>
+</template>
+
+<script setup>
+import ComponentB from './ComponentB.vue'
+</script>
+
+<style scoped></style>
---
+++ src/ComponentB.vue
@@ -0,0 +1,9 @@
+<template>
+  <div style="background-color: lightblue; padding-left: 10px">
+    Grandchild component.
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
